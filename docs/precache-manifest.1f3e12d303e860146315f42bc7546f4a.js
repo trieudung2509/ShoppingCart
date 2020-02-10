7713026fc1ad575629d87c76da6470d81df85631
Dung TV <dungtv3@rikkeisoft.com>
2020-02-10 10:38:55 +0700

    update docs

---
+++ docs/precache-manifest.1f3e12d303e860146315f42bc7546f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53ab615c5ffeba3c13a11625dd2c90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a2c7fa4f2679db05657",
+    "url": "/static/css/main.df95cf32.chunk.css"
+  },
+  {
+    "revision": "3803ca91859906eb1955",
+    "url": "/static/js/2.a650afc4.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.a650afc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a2c7fa4f2679db05657",
+    "url": "/static/js/main.d4c7c0a3.chunk.js"
+  },
+  {
+    "revision": "81111adadbd608461057",
+    "url": "/static/js/runtime-main.5d77b902.js"
+  }
+]);
